55f641ae3e02309f08a918ea9f52511818be6c43
Rituraj  Pal <121292039+rituraj-pal@users.noreply.github.com>
2025-07-12 07:45:09 +0000

    currencyConverter

---
+++ currencyconvertor/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,20 @@
+import { useState, useEffect } from "react";
+
+function useCurrencyInfo(currency) {
+    const [data,setData]=useState(null)
+    useEffect(() => {
+        fetch(`https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`)
+
+        .then((res)=>res.json())
+        .then((res)=>{
+            setData(res[currency])
+             console.log(res[currency])}
+        )
+       
+    }, [currency])
+    // console.log(data);
+    return data
+}
+
+export default useCurrencyInfo;
+
